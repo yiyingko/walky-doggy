d6c0861eb8a3755ecd2e0e803882aeb582a35200
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-03-13 17:05:56 +0100

    feat: Event test cypress

--- client/components/Event.cy.js
+++ client/components/Event.cy.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import Event from './Event'
 
+const date = '04:30 PM - March 16th, 2023'
+
 describe('<Event />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Event />)
+  beforeEach(() => {
+    cy.mount(<Event event ={date}/>)
+  })
+  it('Should render the event with date', () => {
+    cy.mount(<Event event ={date}/>)
+  })
+  it('Should have a click button', () => {
+    cy.get('.btn').click()
   })
 })
