1350c6276a105c3477664a71a9868754e38d75ab
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-03-13 16:56:41 +0100

    Feat: AddRecord cypress test

--- client/components/AddRecord.cy.js
+++ client/components/AddRecord.cy.js
@@ -2,8 +2,21 @@ import React from 'react'
 import AddRecord from './AddRecord'
 
 describe('<AddRecord />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
+  beforeEach(() => {
     cy.mount(<AddRecord />)
+  })
+  it('Should have title', () => {
+    cy.get('h1').should('have.text', 'POO/PEE RECORD');
+  })
+  it('Should have the label on the checkbox', () => {
+    cy.get('.adjustfont').should('have.text', 'PEE');
+    cy.get(':nth-child(2) > .submit-form-control > label').should('have.text', 'POO');
+  })
+  it('Should check the checkboxs', () => {
+    cy.get(':nth-child(1) > :nth-child(1) > input').check();
+    cy.get(':nth-child(2) > .submit-form-control > input').check();
+  })
+  it('Should submit the form', () => {
+    cy.get('form').click()
   })
 })
