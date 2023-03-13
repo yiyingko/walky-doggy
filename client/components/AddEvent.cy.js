89686974d5fd06b2bedaf4cefce7445706a4e43c
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-03-13 16:47:36 +0100

    Feat: AddEvent Cypress test

--- client/components/AddEvent.cy.js
+++ client/components/AddEvent.cy.js
@@ -2,8 +2,21 @@ import React from 'react'
 import AddEvent from './AddEvent'
 
 describe('<AddEvent />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
+  beforeEach(() => {
     cy.mount(<AddEvent />)
+  })
+  it('Should have all the labels', () => {
+    cy.get('h1').should('have.text', 'Book a Walk');
+    cy.get('.add-form > :nth-child(2) > label').should('have.text', 'DOG NAME');
+    cy.get('.add-form > :nth-child(3) > label').should('have.text', 'DATE');
+    cy.get('.add-form > :nth-child(4) > label').should('have.text', 'PICK-UP LOCATION');
+  })
+  it('Should complete the form and submit', () => {
+    cy.get(':nth-child(2) > input').type('Rex')
+    cy.get('.date-picker').click();
+    cy.contains('30').click();
+    cy.contains('3:00 PM').click();
+    cy.get(':nth-child(4) > input').type("Carrer d'avila, 27");
+    cy.get('form').click()
   })
 })
