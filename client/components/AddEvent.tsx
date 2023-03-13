c67da343d0835a2ba276f7c189df0a3c321f1583
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-03-13 21:30:25 +0100

    Feat: TS AddEvent

--- client/components/AddEvent.tsx
+++ client/components/AddEvent.tsx
@@ -1,70 +1,80 @@
-import { useState } from "react";
-import DatePicker from "react-datepicker";
+import { useState } from 'react';
+import DatePicker, { ReactDatePickerProps } from 'react-datepicker';
 
-import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker.css';
 
 type EventProps = {
-  _id: string;
+  _id?: string;
   title: string;
   date: Date;
   venue: string;
 };
 
-const AddEvent = ({ onAdd }) => {
-  const [title, setTitle] = useState<string>("");
-  const [date, setDate] = useState<string>("");
-  const [venue, setVenue] = useState<string>("");
+type AddEventProps = {
+  onAdd: (event: EventProps) => void;
+};
 
+const AddEvent = ({ onAdd }: AddEventProps) => {
+  const [title, setTitle] = useState<string>('');
+  const [date, setDate] = useState<Date | null>(null);
+  const [venue, setVenue] = useState<string>('');
 
-  const onSubmit = (e) => {
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    onAdd({ title, date, venue });
-    setTitle("");
-    setDate("");
-    setVenue("");
+    if (date !== null) {
+      onAdd({ title, date, venue });
+      setTitle('');
+      setDate(null);
+      setVenue('');
+    }
+  };
+
+  const handleDateChange: ReactDatePickerProps['onChange'] = (date: Date) => {
+    setDate(date);
   };
 
   return (
-    <form className="add-form" onSubmit={onSubmit}>
-      <div className="form-title">
+    <form className='add-form' onSubmit={onSubmit}>
+      <div className='form-title'>
         <h1>Book a Walk</h1>
       </div>
-      <div className="form-control">
+      <div className='form-control'>
         <label>DOG NAME</label>
         <input
-          type="text"
-          name="title"
-          placeholder="your dog's name" required
+          type='text'
+          name='title'
+          placeholder="your dog's name"
+          required
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
-        
       </div>
-      <div className="form-control">
+      <div className='form-control'>
         <label>DATE</label>
-        <DatePicker className="date-picker"
-          showTimeSelect required
+        <DatePicker
+          className='date-picker'
+          showTimeSelect
+          required
           selected={date}
-          onSelect={(date) => setDate(date)} //when day is clicked
-          onChange={(date) => setDate(date)} //only when value has changed 
-          dateFormat="Pp"
+          onSelect={handleDateChange} //when day is clicked
+          onChange={handleDateChange} //only when value has changed
+          dateFormat='Pp'
         />
-
-
       </div>
-      <div className="form-control">
+      <div className='form-control'>
         <label>PICK-UP LOCATION</label>
-        <input 
-          type="text"
-          name="venue"
-          placeholder="pick up address" required
+        <input
+          type='text'
+          name='venue'
+          placeholder='pick up address'
+          required
           value={venue}
           onChange={(e) => setVenue(e.target.value)}
         />
       </div>
 
-      <input type="submit" value="BOOK" className="btn-block" />
+      <input type='submit' value='BOOK' className='btn-block' />
     </form>
   );
 };
